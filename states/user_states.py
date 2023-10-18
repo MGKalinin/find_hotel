6de5db341876b7755c110f1a72c4292e40e703e6
Попов Максим <1953115@gmail.com>
2023-10-18 22:38:52 +0300

    restart...

--- states/user_states.py
+++
@@ -1,7 +0,0 @@
-from telebot.handler_backends import State, StatesGroup
-
-
-class UserInfoState(StatesGroup):
-    input_city = State()  # город, который ввёл пользователь
-    # date_of_entry = State()  # дата заезда
-    # departure_date = State()  # дата выезда
