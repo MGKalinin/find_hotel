258782ddbaea03ffadb0ffc6ff561bb6548d5a10
Попов Максим <1953115@gmail.com>
2023-11-09 22:27:55 +0300

    don`t work min price

--- handlers/choosing_a_city.py
+++ handlers/choosing_a_city.py
@@ -1,4 +1,6 @@
 from aiogram import F, Router, types
+from aiogram.filters import StateFilter
+from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import Message
 
@@ -9,20 +11,6 @@ from utilities.find_destination_id import destination_city, destination_hotel
 router = Router()
 user_data = {}
 
-# Есть понимание, что каждой записи из available cities
-# надо сопоставить какой-нибудь айди и где-то это хранить. Ну например:
-#
-# 1 - Москва
-# 2 - Вашингтон
-# 3 - Рим
-# 4 - Сызрань
-
-# Создать объект callbackdata с общим префиксом. Например, city
-
-# И при генерации клавиатуры тогда в кнопках будет зашито
-# "city:1"
-# "city:2"
-# "city:3"
 possible_cities = {'553248633938945217': 'Rome City Centre, Rome, Lazio, Italy', '3023': 'Rome, Lazio, Italy',
                    '5194566': 'Rome, Italy (FCO-Fiumicino - Leonardo da Vinci Intl.)',
                    '6200441': 'Rome Historic Centre, Rome, Lazio, Italy',
@@ -80,35 +68,35 @@ class ChoosDestination(StatesGroup):
     choos_max_price = State()
 
 
-# TODO написать в меню бота /cansel, /minimum_price?
-@router.message(F.text)
-async def find_city(message: Message):
+# написать в меню бота /cansel, /minimum_price?
+# 1.после команды /start запрос ввести город
+# 2.запрос выбрать город
+@router.message()  # F.text
+async def find_city(message: Message, state: FSMContext):
     await message.answer(text="Выберите город:",
-                         reply_markup=get_keyboard_city(destination_city(message.text)))
-    await message.answer(text="Введите максимальную цену:")
-    await message.answer(f"Вы ввели {message.text}")
+                         reply_markup=get_keyboard_city(possible_cities))
+    # destination_city(message.text))
 
 
-# TODO запросить max или min цену- которую передать в
-#  функцию поиска отелей: типа в список- 0:id город,1:max или min цена?
+# 3.запрос ввести минимальную цену :
+# a.вывести две кнопки  "цена до 200$", "цена более 200$"
+# b.запрос "введите максимальную цену"
+# c.запрос "введите минимальную цену"
 
 
-@router.message(F.text)
-async def price_limit(message: Message):
-    await message.answer(text="Введите минимальную цену:")
-    await message.answer(f"Вы ввели {message.text}")
-
-
+# 4.вывод гостиниц в выбранном городе
 @router.callback_query(NumbersCallbackFactory.filter())
 async def find_hotel(
-        callback: types.CallbackQuery,
-        callback_data: NumbersCallbackFactory
-):
+                     callback: types.CallbackQuery,
+                     callback_data: NumbersCallbackFactory
+                     ):
     # Текущее значение
-    print(f'callback_data {callback_data},{str(callback_data)}')
-    # callback_data id_city='3023',id_city='3023'
+    print(f'callback_data {callback_data}')
+    # callback_data id_city='3023'
 
     await callback.message.edit_text(text="Выберите отель:",
-                                     reply_markup=get_keyboard_city(destination_hotel(id_city=str(callback_data))))
+                                     reply_markup=get_keyboard_city(possible_hotels))
+    # destination_hotel(id_city=str(callback_data)))
 
     await callback.answer()
+
